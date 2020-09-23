1ebc3a119f349a9ec4fecc3a73d2ea8008caddc2
lanxinyan <lanxinyan@didiglobal.com>
2020-09-24 00:13:46 +0800

    爬楼梯:忘记前两层楼梯的答案了

--- src/java_sub/ClimbStairs.java
+++ src/java_sub/ClimbStairs.java
@@ -3,6 +3,9 @@ package java_sub;
 public class ClimbStairs {
 
     public int climbStairs(int n) {
+        if (n < 2) {
+            return n;
+        }
         int a = 1, b = 1, c = 2;
         for (int i = 2; i <= n; i++) {
             c = a + b;
