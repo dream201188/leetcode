eedbe790e641e8f17c824a066c979a46d96c9b9f
lanxinyan <lanxinyan@didiglobal.com>
2020-09-29 11:24:06 +0800

    异位词：排序后比较数组

---
+++ src/java_sub/242.有效的字母异位词.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=242 lang=java
+ *
+ * [242] 有效的字母异位词
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+
+        return Arrays.equals(sArray, tArray);
+
+    }
+}
+// @lc code=end
