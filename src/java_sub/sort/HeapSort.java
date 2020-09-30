6dfc1fae83395da7fdaaa92a0342dedb72ffb68a
lanxinyan <lanxinyan@didiglobal.com>
2020-09-30 21:52:52 +0800

    堆排序

--- src/java_sub/sort/HeapSort.java
+++ src/java_sub/sort/HeapSort.java
@@ -1,4 +1,4 @@
-package java_sub;
+package java_sub.sort;
 
 public class HeapSort {
 
@@ -34,24 +34,5 @@ public class HeapSort {
             heapify(arr, n, largest);
         }
 
-    }
-
-    /* A utility function to print array of size n */
-    static void printArray(int arr[]) {
-        int n = arr.length;
-        for (int i = 0; i < n; ++i)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-    }
-
-    // Driver program
-    public static void main(String args[]) {
-        int arr[] = { 3, 12, 11, 13, 8, 5, 6, 14, 7 };
-
-        HeapSort ob = new HeapSort();
-        ob.sort(arr);
-
-        System.out.println("Sorted array is");
-        printArray(arr);
     }
 }
---
+++ src/java_sub/utils/Utils.java
@@ -0,0 +1,18 @@
+package java_sub.utils;
+
+public class Utils {
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    /* A utility function to print array of size n */
+    public static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+}
