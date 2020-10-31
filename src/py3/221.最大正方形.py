08a193b868ad364825ebb226590dd487e3ebee7a
lanxinyan <lanxinyan@didiglobal.com>
2020-10-31 23:43:14 +0800

    最大正方形

--- src/py3/221.最大正方形.py
+++ src/py3/221.最大正方形.py
@@ -1,3 +1,4 @@
+class Solution(object):
     """
     dp(i,j) 表示以 (i, j)(i,j) 为右下角，且只包含 11 的正方形的边长最大值
     对于每个位置 (i, j)(i,j)，检查在矩阵中该位置的值：
@@ -5,9 +6,6 @@
     如果该位置的值是 11，则 dp(i, j)dp(i,j) 的值由其上方、左方和左上方的三个相邻位置的 dpdp 值决定。具体而言，当前位置的元素值等于三个相邻位置的元素中的最小值加 11，状态转移方程如下：
     dp(i, j)=min(dp(i−1, j), dp(i−1, j−1), dp(i, j−1))+1
     """
-
-
-class Solution(object):
 
     def maximalSquare(self, matrix):
         """
