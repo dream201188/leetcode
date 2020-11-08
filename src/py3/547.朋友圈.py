771a272bd982eeca57b5e359dfb7815b5012f49a
lanxinyan <lanxinyan@didiglobal.com>
2020-11-08 22:51:24 +0800

    并查集

---
+++ src/py3/547.朋友圈.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    def findCircleNum(self, M: List[List[int]]) -> int:
+        if not M:
+            return 0
+        n = len(M)
+        p = [i for i in range(n)]
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                if M[i][j] == 1:
+                    self._union(p, i, j)
+        return len(set([self._parent(p, i) for i in range(n)]))
+
+    def _union(self, p, i, j):
+        p1 = self._parent(p, i)
+        p2 = self._parent(p, j)
+        p[p2] = p1
+
+    def _parent(self, p, i):
+        root = i
+        while p[root] != root:
+            root = p[root]
+        while p[i] != i:
+            x = i
+            i = p[i]
+            p[x] = root
+        return root
