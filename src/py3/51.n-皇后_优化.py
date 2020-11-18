9c91e996916e42ecc445ea40e63e4f0ae5c6541d
lanxinyan <lanxinyan@didiglobal.com>
2020-11-18 23:25:05 +0800

    用位运算解决n皇后

--- src/py3/51.n-皇后_优化.py
+++ src/py3/51.n-皇后_优化.py
@@ -31,5 +31,28 @@ class Solution(object):
         dfs([], [], [], result)
         return [['.' * col + 'Q' + '.' * (n - col - 1) for col in path] for path in result]
 
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+
+        def dfs(path,cols, pie, na):
+            row = len(path)
+            if row == n:
+                result.append(path)
+                return
+
+            availablePositions = ((1 << n) - 1) & (~(cols | na | pie)) # pie na 三个数1 表示皇后位置，取反后1表示可以放的位置
+            while availablePositions:
+                position = availablePositions & (-availablePositions) #获取最后一个1；
+                availablePositions = availablePositions & (availablePositions - 1) # 将最后一个1变成0
+                col_index = bin(position - 1).count("1")
+                dfs(path + [col_index], cols | position, (pie | position) >> 1, (na | position) << 1)
+
+        path,result = [], []
+        dfs(path, 0, 0, 0)
+        return [['.' * col + 'Q' + '.' * (n - col - 1) for col in path] for path in result]
+
 
 # @lc code=end
